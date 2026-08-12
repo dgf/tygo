5ecd7f4644163f373e8d0d5734ccc28b82de1fb6
dgf <deep@dagnu.de>
2026-08-12 21:33:53 +0100

    ignore print returns

--- internal/display/result.go
+++ internal/display/result.go
@@ -10,9 +10,13 @@ import (
 func PrintResult(out io.Writer, result test.Result) {
 	NewLine(out)
 	NewLine(out)
-	fmt.Fprintf(out, "Result: %s", result)
+
+	_, _ = fmt.Fprintf(out, "Result: %s", result)
+
 	NewLine(out)
 	NewLine(out)
-	fmt.Fprint(out, "[ENTER] next or [ESC] to quit")
+
+	_, _ = fmt.Fprint(out, "[ENTER] next or [ESC] to quit")
+
 	NewLine(out)
 }
