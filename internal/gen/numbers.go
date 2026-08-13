e07e945165798bf2c03c53d5c23f4e6978d64717
dgf <deep@dagnu.de>
2026-08-13 15:54:32 +0100

    lint number gen

--- internal/gen/numbers.go
+++ internal/gen/numbers.go
@@ -1,20 +1,20 @@
 package gen
 
 import (
-	"math/rand"
 	"slices"
 	"strconv"
 )
 
+const MaxRandomNumber = 9999
+
 func WithNumbers(weight int, words []string) []string {
 	result := slices.Clone(words)
+	count := len(words)
+	dist := map[bool]int{false: 100 - weight, true: weight}
 
-	for i, b := range SampleWeightedDist(len(words), map[bool]int{
-		false: 100 - weight,
-		true:  weight,
-	}) {
+	for i, b := range SampleWeightedDist(count, dist) {
 		if b {
-			result[i] = strconv.Itoa(rand.Intn(9999) + 1)
+			result[i] = strconv.Itoa(randGen.Intn(MaxRandomNumber) + 1)
 		}
 	}
 
